8b96b69f78513edc79a6fcf2ea91ec8cf52b9614
Gabriel M. Bermudez <gabrielbermudez0@gmail.com>
2022-04-21 23:20:36 -0300

    Subo desafio 1

---
+++ Desafio1.py
@@ -0,0 +1,6 @@
+nota1 = input("Ingrese la primer nota: ")
+nota2 = input("Ingrese la segunda nota: ")
+
+promedio = 0.6 * int(nota1) + 0.4 * int(nota2)
+
+print(f"El valor de la nota 1 es: {nota1} y el valor de la nota 2 es {nota2}, su prometio seria: {promedio}")
